bb925e0b70c0bacdd6c40b4ea153e0c499d44c0a
JoseParra28 <iliftandcook@gmail.com>
2022-10-05 18:06:28 +0000

    set up first funtion on js

--- assets.folder/script.js
+++ assets.folder/script.js
@@ -1,1 +1,24 @@
-console.log ("conected!")
+function runGame () {
+
+}
+function checkAnswer () {
+
+}
+function calculateCorrectAnswer () {
+
+}
+function incrementScore () {
+
+}
+function incrementWrongAnswer () {
+
+}
+function displayAdditionQuestion () {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion () {
+
+}
