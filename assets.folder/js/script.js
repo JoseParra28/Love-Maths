d82cc91b247c2c93681b862d95d9c4b6d3cd5a52
JoseParra28 <iliftandcook@gmail.com>
2023-06-16 05:37:16 +0000

    save

--- assets.folder/js/script.js
+++ assets.folder/js/script.js
@@ -17,3 +17,44 @@ toggleBtn.addEventListener("click", function(){
 closeBtn.addEventListener("click", function(){
     sidebar.classList.remove("show-sidebar")
 })
+
+// ----------------------card
+const magicBtn = document.querySelector(".magic-btn");
+const openModal = document.querySelector(".book");
+const closeModal = document.querySelector(".close-modal");
+
+magicBtn.addEventListener("click", function () {
+    openModal.classList.remove("open-modal");
+  });
+closeModal.addEventListener("click", function(){
+    openModal.classList.add("open-modal");
+})
+
+// ----------------------questions (1)
+// traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function(btn){
+// btn.addEventListener("click", function(e){
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text")
+// })
+// })
+
+// ----------------------questions (2)
+// using selector inside element
+
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(function(question){
+    const btn = question.querySelector(".question-btn")
+    btn.addEventListener("click", function(){
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle("show-text");
+    })
+});
